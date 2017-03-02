e00621efc7f0ef61c477bb8b6de88d996ab5db51
n.yakovenko <n.yakovenko@smartweb.com.ua>
2017-03-02 15:10:03 +0200

    added lives counter

--- hang_man/hang_man.py
+++ hang_man/hang_man.py
@@ -2,7 +2,7 @@ import getpass
 
 class HangMan():
     GAME_STEPS = 5
-    DEFAULT_USER_LIVES = 5
+    USER_LIVES = 5
 
     @classmethod
     def game_procces(cls):
@@ -10,36 +10,44 @@ class HangMan():
         partly_visible_word = ['*'] * len(user_word)
         asterisks_list = cls.make_asterisks_from_word(user_word)
         asterisks_list = ''.join(asterisks_list)
+        user_lives_count = None
 
-        print('Word to guess: {}'.format(asterisks_list))
+        print('Word to guess: {}\n'.format(asterisks_list))
 
         for step in range(cls.GAME_STEPS):
-            letters = cls.show_guessed_letters(user_word)
+            if step == 0:
+                user_lives_count = cls.USER_LIVES
+
+            user_letter = cls.ask_user_to_type_letter(user_lives_count)
+            guessed_letters = cls.show_guessed_letters(user_word, user_letter)
+
+            if not user_letter or user_letter not in user_word:
+                user_lives_count -= 1
 
             # save all guessed letters and show it on next steps
-            for i, symbol in enumerate(letters):
+            for i, symbol in enumerate(guessed_letters):
                 if symbol != '*':
                     partly_visible_word[i] = partly_visible_word[i].replace(partly_visible_word[i], symbol)
 
             result = ''.join(partly_visible_word)
 
             if user_word == result:
-                return print('You win the game!\nThis word is - {}'.format(user_word))
-            elif step == cls.GAME_STEPS - 1:
-                return print('You lose the game.')
+                return print('\nYou win the game!\nThis word is - {}.'.format(user_word))
+            elif step == cls.GAME_STEPS or user_lives_count == 0:
+                return print('Game over!')
             else:
-                print('Word to guess: {}'.format(result))
+                print('\nWord to guess: {}'.format(result))
 
     @classmethod
     def ask_user_to_type_word(cls):
         word = getpass.getpass(prompt='Type a word and press Enter: ')
-        return word
+        return word.lower()
 
 
     @classmethod
-    def ask_user_to_type_letter(cls):
-        next_letter = input('You have xxxx lives left - Letter? ')
-        return next_letter
+    def ask_user_to_type_letter(cls, lives):
+        next_letter = input('You have {} lives left - Letter? '.format(lives))
+        return next_letter.lower()
 
 
     @classmethod
@@ -57,8 +65,7 @@ class HangMan():
 
 
     @classmethod
-    def show_guessed_letters(cls, word):
-        user_letter = cls.ask_user_to_type_letter()
+    def show_guessed_letters(cls, word, user_letter):
         if user_letter in word:
             guessed_letters = cls.replace_asterisk_on_letter(word, user_letter)
             return guessed_letters
